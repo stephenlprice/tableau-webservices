13bb9a2a77fab0ad72005631e34397def3eeca11
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-11-09 00:49:05 -0600

    finalized workflow: request current weather data and create a dataframe

--- modules/data.py
+++ modules/data.py
@@ -1,8 +1,18 @@
-import pandas
-from libs import rest
+import pandas as pd
+import json
+from libs import rest, transform
+
+# creates a data frame from forecast data
+def current(api_key):
+  payload = rest.current(api_key)
+  data = transform.current(payload)
+
+  return data
+
 
 # creates a data frame from forecast data
 def forecast(api_key):
-  data = rest.forecast(api_key)
+  payload = rest.forecast(api_key)
+  data = transform.forecast(payload)
   
-  print(data)
+  return data
