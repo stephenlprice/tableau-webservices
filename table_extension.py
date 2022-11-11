01c1511b561d5870a0ec4b772a7966c628eab305
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-11-11 00:31:30 -0600

    outputting a dataframe with data for several cities

--- table_extension.py
+++ table_extension.py
@@ -2,85 +2,104 @@ def current_weather(cities):
   import requests
   import pandas as pd
 
-  api_key = "API Key"
+  api_key = "api key"
 
   # creates a data frame from current weather data
-  def current(api_key, cities):
-    payload = rest_current(api_key, cities)
-    data = transform_current(payload, cities)
-    return data
+  # def current(api_key, cities):
+  #   payload = rest_current(api_key, cities)
+  #   data = transform_current(payload)
+  #   return data
 
   # gets current weather data for the specified geolocation
   def rest_current(api_key, cities):
-    url = f'https://api.openweathermap.org/data/2.5/weather?lat=30.2711286&lon=-97.7436995&appid={api_key}&units=imperial'
-    response = requests.get(url)
-    payload = response.json()
-    return payload
+    # a list of current weather data per city
+    city_data = {}
+    # iterate through the cities dict
+    for city in cities:
+      # assign coordinates
+      lon = cities[city]["lon"]
+      lat = cities[city]["lat"]
+      url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=imperial'
+      response = requests.get(url)
+      payload = response.json()
+      city_data[city] = payload
+    return city_data
 
   # creates a dataframe from the JSON payload with current weather
-  def transform_current(payload, cities):
-    # coordinates
-    coord = payload["coord"]
-    coord["ID"] = 1
-    coord = pd.DataFrame.from_dict([coord])
+  def transform_current(city_data):
+    weather_data = pd.DataFrame()
+    for city in city_data:
+      # coordinates
+      coord = city_data[city]["coord"]
+      coord["ID"] = 1
+      coord = pd.DataFrame.from_dict([coord])
 
-    # weather results
-    weather = payload["weather"][0]
-    del weather["id"]
-    weather["ID"] = 1
-    weather = pd.DataFrame.from_dict([weather])
+      # weather results
+      weather = city_data[city]["weather"][0]
+      del weather["id"]
+      weather["ID"] = 1
+      weather = pd.DataFrame.from_dict([weather])
 
-    # main weather data
-    main = payload["main"]
-    main["ID"] = 1
-    main = pd.DataFrame.from_dict([main])
+      # main weather data
+      main = city_data[city]["main"]
+      if "sea_level" in main:
+        del main["sea_level"]
+      if "grnd_level" in main:
+        del main["grnd_level"]
+      main["ID"] = 1
+      main = pd.DataFrame.from_dict([main])
 
-    # visibility 
-    visibility = {}
-    visibility["visibility"] = payload["visibility"]
-    visibility["ID"] = 1
-    visibility = pd.DataFrame.from_dict([visibility])
+      # visibility 
+      visibility = {}
+      visibility["visibility"] = city_data[city]["visibility"]
+      visibility["ID"] = 1
+      visibility = pd.DataFrame.from_dict([visibility])
 
-    # wind
-    wind = {}
-    wind["wind speed"] = payload["wind"]["speed"]
-    wind["wind deg"] = payload["wind"]["deg"]
-    wind["ID"] = 1
-    wind = pd.DataFrame.from_dict([wind])
+      # wind
+      wind = {}
+      wind["wind speed"] = city_data[city]["wind"]["speed"]
+      wind["wind deg"] = city_data[city]["wind"]["deg"]
+      wind["ID"] = 1
+      wind = pd.DataFrame.from_dict([wind])
 
-    # clouds
-    clouds = {}
-    clouds["clouds"] = payload["clouds"]["all"]
-    clouds["ID"] = 1
-    clouds = pd.DataFrame.from_dict([clouds])
+      # clouds
+      clouds = {}
+      clouds["clouds"] = city_data[city]["clouds"]["all"]
+      clouds["ID"] = 1
+      clouds = pd.DataFrame.from_dict([clouds])
 
-    # country
-    country = {}
-    country["country"] = payload["sys"]["country"]
-    country["ID"] = 1
-    country = pd.DataFrame.from_dict([country])
+      # country
+      country = {}
+      country["country"] = city_data[city]["sys"]["country"]
+      country["ID"] = 1
+      country = pd.DataFrame.from_dict([country])
 
-    # name (city)
-    name = {}
-    name["name"] = payload["name"]
-    name["ID"] = 1
-    name = pd.DataFrame.from_dict([name])
+      # name (city)
+      name = {}
+      name["name"] = city_data[city]["name"]
+      name["ID"] = 1
+      name = pd.DataFrame.from_dict([name])
 
-    # joins the dataframes into a single row of data
-    data = pd.merge(coord, weather, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, main, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, visibility, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, wind, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, clouds, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, country, left_on='ID', right_on='ID', sort=False)
-    data = pd.merge(data, name, left_on='ID', right_on='ID', sort=False)
+      # joins the dataframes into a single row of data
+      data = pd.merge(coord, weather, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, main, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, visibility, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, wind, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, clouds, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, country, left_on='ID', right_on='ID', sort=False)
+      data = pd.merge(data, name, left_on='ID', right_on='ID', sort=False)
 
-    data.set_index("ID", drop=True, inplace=True)
-    data = data.to_dict('list')
+      weather_data = pd.concat([weather_data, data])
 
-    return data
+      # weather_data.set_index("ID", drop=True, inplace=True)
+      # weather_data = data.to_dict('list')
 
-  return current(api_key, cities)
+    return weather_data
+
+  payload = rest_current(api_key, cities)
+  data = transform_current(payload)
+
+  return data
 
 cities = {
   "Austin": {
