9b58777894a1e9560842f04a0a20fb7bde1d8a17
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2022-11-10 23:37:59 -0600

    updated rest calls

--- libs/rest.py
+++ libs/rest.py
@@ -2,7 +2,7 @@ import requests
 
 # gets current weather data for the specified geolocation
 def current(api_key):
-  url = f'https://api.openweathermap.org/data/2.5/weather?lat=30.2711286&lon=-97.7436995&appid={api_key}'
+  url = f'https://api.openweathermap.org/data/2.5/weather?lat=30.2711286&lon=-97.7436995&appid={api_key}&units=imperial'
 
   response = requests.get(url)
 
@@ -12,7 +12,7 @@ def current(api_key):
 
 # gets 5 day weather forecast data for the specified geolocation
 def forecast(api_key):
-  url = f'https://api.openweathermap.org/data/2.5/forecast?lat=30.2711286&lon=-97.7436995&appid={api_key}'
+  url = f'https://api.openweathermap.org/data/2.5/forecast?lat=30.2711286&lon=-97.7436995&appid={api_key}&units=imperial'
 
   response = requests.get(url)
 
